54b595cb9caf8ce7a43cbc5880c3d48cd925803b
D3rise <derise.dev@gmail.com>
2025-07-06 12:11:00 +0300

    feat: create repl logic

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
+const POKEDEX_PROMPT = "Pokedex > "
+
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print(POKEDEX_PROMPT)
+	for scanner.Scan() {
+		text := scanner.Text()
+		fmt.Println("Your command was:", cleanInput(text)[0])
+		fmt.Print(POKEDEX_PROMPT)
+	}
 }
 
 func cleanInput(text string) []string {
