43923322073c34b5817602eefebe74707d8d8600
D3rise <derise.dev@gmail.com>
2025-07-06 22:43:17 +0300

    refactor: refactor context

--- internal/context/context.go
+++ internal/context/context.go
@@ -19,34 +19,34 @@ func NewContext() *Context {
 }
 
 func (c *Context) Set(key ContextKey, value any) {
-	(*c).mu.Lock()
-	defer (*c).mu.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
-	(*c).kv[key] = value
+	c.kv[key] = value
 }
 
 func (c *Context) Get(key ContextKey) any {
-	(*c).mu.RLock()
-	defer (*c).mu.RUnlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 
 	return c.kv[key]
 }
 
 func (c *Context) Del(key ContextKey) error {
-	(*c).mu.Lock()
-	defer (*c).mu.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
 	if _, ok := c.kv[key]; !ok {
 		return errors.New("key does not exist")
 	}
 
-	delete((*c).kv, key)
+	delete(c.kv, key)
 	return nil
 }
 
 func (c *Context) Has(key ContextKey) bool {
-	(*c).mu.RLock()
-	defer (*c).mu.RUnlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 
 	_, has := c.kv[key]
 	return has
